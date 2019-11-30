bfecc72dc686ffe5dab82ee209c8a8f15361db1e
Elena Dinca <elenadinca25@gmail.com>
2019-11-30 15:08:54 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,6 @@
-# Write your #display_rainbow method here
+def display_rainbow(colors)
+  puts "#{colors[0]}, #{colors[1]}, #{colors[2]}, #{colors[3]}, #{colors[4]}, #{colors[5]}, #{colors[6]}"
+display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+
+   colors = ['red', 'orange','yellow','green','blue','indigo','violet']
+   expect { display_rainbow(colors) }.to output("R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet\n").to_stdout
